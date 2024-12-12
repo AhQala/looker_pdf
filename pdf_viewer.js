c8d20116b9e5d27dd6efcdab4b619d9622560fda
AhQala <ahmadwqasim1996@gmail.com>
2024-12-12 18:04:46 +0300

    Update pdf_viewer.js

--- pdf_viewer.js
+++ pdf_viewer.js
@@ -3,7 +3,7 @@ looker.plugins.visualizations.add({
     apps_script_url: {
       type: "string",
       label: "Apps Script URL",
-      default: "https://script.google.com/a/macros/google.com/s/AKfycbzVzF_vxS8MlqpvzQoXyD6LOgG5iCmR6YiupooJXGPCnVMtDmCMoV2CppMisCkeWldM/exec",
+      default: "https://script.google.com/a/macros/google.com/s/AKfycbzn62O99FQJiDDsaY056QdZj3kv0aMGK2dcAjrwH9sE_78xBvG8ixHedhpDvUEU4KvRNQ/exec",
       section: "Settings"
     }
   },
@@ -55,32 +55,26 @@ looker.plugins.visualizations.add({
       return doneRendering();
     }
 
-    const pdfUrl = data[0][queryResponse.fields.dimension_like[0].name].value;
-    const fileId = pdfUrl.match(/[-\w]{25,}/)[0];
     const appsScriptUrl = config.apps_script_url;
-    
-    this.container.innerHTML = '<div class="loading-message">Loading PDF...</div>';
+    this.container.innerHTML = '<div class="loading-message">Processing PDF...</div>';
 
-    fetch(`${appsScriptUrl}?fileId=${fileId}`, {
-      method: 'GET',
-      mode: 'cors'
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.error) throw new Error(data.error);
-      
-      this.container.innerHTML = '';
-      data.images.forEach(image => {
-        const img = document.createElement('img');
-        img.src = `data:image/jpeg;base64,${image.data}`;
-        img.className = 'pdf-page';
-        this.container.appendChild(img);
-      });
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      this.container.innerHTML = `<div class="error-message">Error loading PDF: ${error.message}</div>`;
-    })
-    .finally(doneRendering);
+    fetch(appsScriptUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.error) throw new Error(data.error);
+        
+        this.container.innerHTML = '';
+        data.images.forEach(image => {
+          const img = document.createElement('img');
+          img.src = `data:image/jpeg;base64,${image.data}`;
+          img.className = 'pdf-page';
+          this.container.appendChild(img);
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        this.container.innerHTML = `<div class="error-message">Error loading PDF: ${error.message}</div>`;
+      })
+      .finally(doneRendering);
   }
 });
